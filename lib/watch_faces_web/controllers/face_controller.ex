41614da4ce78d0d4a1678aef55775d49e0817956
Daniils Petrovs <dpetrovs@evolution.com>
2021-05-30 22:15:26 +0300

    Implement thumbnail saver

--- lib/watch_faces_web/controllers/face_controller.ex
+++ lib/watch_faces_web/controllers/face_controller.ex
@@ -18,15 +18,9 @@ defmodule WatchFacesWeb.FaceController do
 
   def create(conn, %{"face" => face_params}) do
     pkg_file =
-      if upload = face_params["watchface_file"] do
-        Logger.info("Saving new watch face with id: #{face.id}")
-
-        upload_file_name = pkg_file_name(face_params)
-
-        Logger.debug("Upload file name: #{upload_file_name}")
-
-        :ok = File.cp(upload.path, "/var/www/faces/media/#{upload_file_name}")
-        upload_file_name
+      if face_params["watchface_file"] do
+        save_thumbnail(face_params)
+        save_watchface(face_params)
       end
 
     case Faces.create_face(Map.merge(face_params, %{"pkg_file" => "/uploads/#{pkg_file}"})) do
@@ -76,8 +70,38 @@ defmodule WatchFacesWeb.FaceController do
   end
 
   defp pkg_file_name(%{"author" => author, "name" => name, "watchface_file" => upload}) do
-    #hash = :crypto.hash(:sha256, author <> name) |> Base.encode64()
     extension = Path.extname(upload.filename)
     "#{author}-#{name}-pkg#{extension}" |> URI.encode()
+  end
+
+  defp save_watchface(%{"watchface_file" => upload} = face_params) do
+    upload_file_name = pkg_file_name(face_params)
+
+    Logger.debug("Upload file name: #{upload_file_name}")
+
+    :ok = File.cp(upload.path, "/var/www/faces/media/#{upload_file_name}")
+    upload_file_name
+  end
+
+  defp save_thumbnail(%{"watchface_file" => upload} = face_params) do
+    working_dir = upload.path
+
+    zip_opts = [
+      {:cwd, to_charlist(working_dir)},
+      {:file_list, ['snapshot.png']}
+    ]
+
+    # Extract thumbnail img in the temp dir
+    {:ok, _filelist} = :zip.unzip(to_charlist(upload.filename), zip_opts)
+    thumb_file_name = "#{face_params.author}-#{face_params.name}-thumb.png" |> URI.encode()
+
+    # Copy and rename the extracted thumbnail to uploads folder, then delete old one
+    :ok =
+      File.cp(
+        Path.join([working_dir, "snapshot.png"]),
+        Path.join("/var/www/faces/media/", thumb_file_name),
+        fn _, _ -> true end
+      )
+    :ok = File.rm(Path.join(working_dir, "snapshot.png"))
   end
 end
